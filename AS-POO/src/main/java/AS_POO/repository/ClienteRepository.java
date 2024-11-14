cff332ccbf8624ad7f3d95f8e9ed3c77b23e295d
rhuan barrufi <rhuanbarrufi@gmail.com>
2024-11-13 21:51:53 -0300

    primeiro commit

--- AS-POO/src/main/java/AS_POO/repository/ClienteRepository.java
+++ AS-POO/src/main/java/AS_POO/repository/ClienteRepository.java
@@ -1,4 +1,14 @@
 package AS_POO.repository;
 
-public class ClienteRepository {
-}
+import AS_POO.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+    @Repository
+    public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+        List<Cliente> findByIdade(int idade);
+
+    }
+
--- AS-POO/src/main/java/AS_POO/service/ClienteService.java
+++ AS-POO/src/main/java/AS_POO/service/ClienteService.java
@@ -1,11 +1,13 @@
 package AS_POO.service;
 
 import AS_POO.model.Cliente;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class ClienteService {
     private List<Cliente> clientes = new ArrayList<Cliente>();
 
